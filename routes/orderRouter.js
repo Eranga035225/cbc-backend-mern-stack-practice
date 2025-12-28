0cace6476a23eabc592a83a4e2f9a5d51c91da1c
Eranga <erangakavishanka@icloud.com>
2025-12-28 17:19:57 +0530

    fixed an error

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { createOrder} from "../controllers/orderController.js";
+import { getOrders } from "../controllers/orderController.js";
 
 
 const orderRouter = express.Router();

76647aac50d3565d7a53d2399da7b92b332ccd23
Eranga <erangakavishanka@icloud.com>
2025-12-29 21:33:03 +0530

    develoepd model for otp

--- models/otp.js
+++ models/otp.js
@@ -2,12 +2,19 @@ import mongoose from "mongoose";
 
 const otpSchema = mongoose.Schema({
 
+  email: {
+    type: String,
+    required: true
+  },
 
-
-
-
-
+  otp: {
+    type: String,
+    required: true
+  },
   
+
 })
 
+
+const OTP = mongoose.model('OTP', otpSchema)
 

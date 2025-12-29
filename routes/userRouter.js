5e93748820ee3f30d974926c9dcc2ecf1376cb45
Eranga <erangakavishanka@icloud.com>
2025-12-29 21:55:30 +0530

    added the route also

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,6 +2,7 @@ import express from "express";
 import { createUser, loginUser } from "../controllers/userController.js";
 import { loginWithGoogle } from "../controllers/userController.js";
 import { sendOTP } from "../controllers/userController.js";
+import { resetPassword } from "../controllers/userController.js";
 
 
 
@@ -13,4 +14,5 @@ userRouter.post("/", createUser);
 userRouter.post("/login", loginUser);
 userRouter.post("/login/google", loginWithGoogle);
 userRouter.post("/send-otp", sendOTP);
+userRouter.post("/reset-password", resetPassword)
 export default userRouter;

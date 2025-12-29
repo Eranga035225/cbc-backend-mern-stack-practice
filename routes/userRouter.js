dcc8b434d5d0fbbd41a0b0e7d137c9c9f10f85cc
Eranga <erangakavishanka@icloud.com>
2025-12-29 19:32:24 +0530

    added royte

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { createUser, loginUser } from "../controllers/userController.js";
 import { loginWithGoogle } from "../controllers/userController.js";
+import { sendOTP } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/", createUser);
 userRouter.post("/login", loginUser);
 userRouter.post("/login/google", loginWithGoogle);
+userRouter.post("/send-otp", sendOTP);
 export default userRouter;

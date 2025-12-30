72feffbcb6f97287fdd19e1c221f312e92b03c06
Eranga <erangakavishanka@icloud.com>
2025-12-30 17:04:27 +0530

    input validation

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,8 +1,24 @@
-export default function addReview(req,res){
+export async  function addReview(req,res){
+
+  if(req.user == null){
+    res.status(403).json({
+      message: "Please login first"
+    })
+  }
+
+  const [productId,rating,comment ] = req.body;
+
+  if(!productId || !rating || !comment){
+    res.status(400).json({
+      message: "Missing required fields"
+    })
+  }
 
 
 
 
 
-  
+
+
+
 }

92c143b4c405d6c7ab710afbf13ba57c95d48eaf
Eranga <erangakavishanka@icloud.com>
2025-12-30 17:08:33 +0530

    develoepd the function to add reviews

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,5 +1,11 @@
+import Review from "../models/review.js";
+
+
 export async  function addReview(req,res){
 
+  try{
+
+    
   if(req.user == null){
     res.status(403).json({
       message: "Please login first"
@@ -14,6 +20,42 @@ export async  function addReview(req,res){
     })
   }
 
+  const review = new Review(
+    {
+
+      productId : req.body.productId,
+      userName : req.user.firstName + " " + req.user.lastName,
+      rating : req.body.rating,
+      comment : req.body.comment
+    }
+  )
+
+ response =  await review.save();
+
+ if(response){
+  res.json({
+    message: "Review added successfully"
+  })
+ }else{
+  res.status(500).json({
+    message: "Failed to add the review"
+  })
+ }
+
+  }catch(err){
+
+    res.status(500).json({
+      message: "Failed to add the review",
+      error : err
+    })
+ }
+
+
+
+  }
+
+
+  
 
 
 
@@ -21,4 +63,6 @@ export async  function addReview(req,res){
 
 
 
-}
+
+
+
